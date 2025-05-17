f759a91630349e838228abc76f0cd01a7342edc7
sandeep yadav <sandeepyada234abc@gmail.com>
2025-05-17 08:26:05 +0530

    Add Project Files

---
+++ routes/listing.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const Listing = require("../models/listing.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+const multer  = require('multer');
+const {storage} = require("../cloudConfig.js");
+const upload = multer({ storage });
+
+const listingController = require("../controllers/listings.js")
+
+// index && create route
+router
+.route("/")
+.get(wrapAsync(listingController.index))
+.post(
+    isLoggedIn,
+    upload.single('listing[image]'),
+    validateListing,
+    wrapAsync(listingController.createListing)
+);
+
+//new Route
+router.get("/new", isLoggedIn, listingController.renderNewForm);
+
+//show && update && delete Route
+router.route("/:id")
+.get(wrapAsync(listingController.showListing))
+.put(
+    isLoggedIn,
+    isOwner,
+    upload.single('listing[image]'),
+    validateListing,
+    wrapAsync(listingController.updateListing))
+.delete(
+        isLoggedIn,
+        isOwner,
+        wrapAsync(listingController.destroyListing));
+
+//Edit Route
+router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
+    
+    module.exports = router;
+
